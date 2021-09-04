1ddfc77d5e2099af3267f02c34acf77b80cb4075
jwcom1995 <95god95@gmail.com>
2021-09-04 21:40:17 +0900

    create commit() method

--- ShoppingMall_Manager02/src/common/JDBCTemplate.java
+++ ShoppingMall_Manager02/src/common/JDBCTemplate.java
@@ -5,28 +5,37 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCTemplate {
-public static Connection getConnection() {
-		
+	public static Connection getConnection() {
+
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 		} catch (ClassNotFoundException e) {
 			System.out.println("[Error] 드라이버 등록 실패");
 			e.printStackTrace();
 		}
-		
-		String url ="jdbc:oracle:thin:@localhost:1521:XE";
+
+		String url = "jdbc:oracle:thin:@localhost:1521:XE";
 		String id = "JW";
 		String pw = "JW";
-		
+
 		Connection con = null;
-		
+
 		try {
-			con=DriverManager.getConnection(url,id,pw);
+			con = DriverManager.getConnection(url, id, pw);
 			con.setAutoCommit(false);
 		} catch (SQLException e) {
 			System.out.println("[Error] 오라클 연결 실패");
 			e.printStackTrace();
 		}
 		return con;
+	}
+
+	public static void commit(Connection con) {
+		try {
+			con.commit();
+		} catch (SQLException e) {
+			System.out.println("[Error] commit 에러");
+			e.printStackTrace();
+		}
 	}
 }
